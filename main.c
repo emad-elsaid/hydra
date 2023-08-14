bfc6d7829bd16a4e8c71ab2cd19d99f66b400750
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2023-08-14 19:55:52 +0200

    initial version

---
+++ main.c
@@ -0,0 +1,156 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+typedef struct Command {
+  char key;
+  char* name;
+  char* command;
+
+  struct Command* next;
+} Command;
+
+Command* NewCommand(char key, char* name, char* command) {
+  Command* cmd = (Command*) malloc(sizeof(Command));
+  cmd->key = key;
+  cmd->name = name;
+  cmd->command = command;
+  cmd->next = NULL;
+
+  return cmd;
+}
+
+typedef struct Group {
+  char key;
+  char* name;
+
+  struct Group* children;
+  Command* commands;
+
+  struct Group* next;
+} Group;
+
+Group* NewGroup(char key, char* name) {
+  Group *grp = (Group*) malloc(sizeof(Group));
+  grp->key = key;
+  grp->name = name;
+  grp->children=NULL;
+  grp->commands=NULL;
+  grp->next=NULL;
+
+  return grp;
+}
+
+
+void PrintGroup(Group* g) {
+  printf("> %s\n", g->name);
+
+  Group* child = g->children;
+  while(child) {
+    printf("%c %s\t\t", child->key, child->name);
+    child = child->next;
+  }
+
+  Command* command = g->commands;
+  while(command) {
+    printf("%c %s\t\t", command->key, command->name);
+    command = command->next;
+  }
+
+
+  printf("\n");
+}
+
+void GroupAddChild(Group* g, Group* child) {
+  Group* lastChild = g->children;
+  if(lastChild == NULL){
+      g->children = child;
+      return;
+  }
+
+  while(lastChild->next!=NULL) lastChild = lastChild->next;
+  lastChild->next = child;
+}
+
+void GroupAddCommand(Group* g, Command* cmd) {
+  Command* lastCommand = g->commands;
+  if( lastCommand == NULL ) {
+    g->commands = cmd;
+    return;
+  }
+
+  while(lastCommand->next!=NULL) lastCommand = lastCommand->next;
+  lastCommand->next = cmd;
+}
+
+Group *FindGroup(Group *g, char key) {
+  Group *child = g->children;
+  while (child != NULL && child->key != key)
+    child = child->next;
+  return child;
+}
+
+Command *FindCommand(Group *g, char key) {
+  Command* child = g->commands;
+  while( child != NULL && child->key != key) child = child->next;
+  return child;
+}
+
+// Copied from: https://stackoverflow.com/a/912796/458436
+char getch(void) {
+  struct termios old = {0};
+
+  if (tcgetattr(0, &old) < 0) perror("tcsetattr()");
+
+  old.c_lflag &= ~ICANON;
+  old.c_lflag &= ~ECHO;
+  old.c_cc[VMIN] = 1;
+  old.c_cc[VTIME] = 0;
+
+  if (tcsetattr(0, TCSANOW, &old) < 0) perror("tcsetattr ICANON");
+
+  char buf = 0;
+  if (read(0, &buf, 1) < 0) perror("read()");
+
+  old.c_lflag |= ICANON;
+  old.c_lflag |= ECHO;
+
+  if (tcsetattr(0, TCSADRAIN, &old) < 0) perror("tcsetattr ~ICANON");
+  return (buf);
+}
+
+Group* BuildRoot(void) {
+  Group *root = NewGroup('-', "Root");
+
+  Group *git = NewGroup('g', "Git");
+  GroupAddChild(root, git);
+
+  Command *git_status = NewCommand('s', "Status", "git status");
+  GroupAddCommand(git, git_status);
+
+  return root;
+}
+
+int main() {
+  Group* currentGroup = BuildRoot();
+
+  while(true) {
+    PrintGroup(currentGroup);
+
+    char choice = getch();
+    Group* nextGroup = FindGroup(currentGroup, choice);
+    Command* nextCommand = FindCommand(currentGroup, choice);
+
+    if ( nextGroup != NULL ) {
+      currentGroup = nextGroup;
+    } else if ( nextCommand != NULL ) {
+      system(nextCommand->command);
+      break;
+    } else {
+      break;
+    }
+  }
+  return 0;
+}
