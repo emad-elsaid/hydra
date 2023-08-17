2588b513566a9713f225c601dcad2c13f4a876b3
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2023-08-17 09:05:58 +0200

    remove comment

--- hydra-completion.bash
+++ hydra-completion.bash
@@ -2,7 +2,6 @@
 
 # Copied from Stackoverflow: https://stackoverflow.com/a/76917497/458436
 _hydra(){
-    # short variable names are easier to read
     local -n ln=READLINE_LINE pt=READLINE_POINT
 
     # if line is not empty, just insert a space
@@ -12,10 +11,7 @@ _hydra(){
         return
     fi
 
-    # do stuff here
     make run
-
-    # no need to re-print the prompt
 }
 
 bind -x '" ":_hydra'
