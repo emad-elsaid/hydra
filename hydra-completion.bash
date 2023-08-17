347c09ba60d501320f1c2c31f689da905fc92919
Emad Elsaid <emad.elsaid.hamed@gmail.com>
2023-08-17 09:05:26 +0200

    add bash completion script

--- hydra-completion.bash
+++ hydra-completion.bash
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
 
-cmd_complete () {
+# Copied from Stackoverflow: https://stackoverflow.com/a/76917497/458436
+_hydra(){
+    # short variable names are easier to read
+    local -n ln=READLINE_LINE pt=READLINE_POINT
+
+    # if line is not empty, just insert a space
+    if [[ -n $ln ]]; then
+        ln="${ln:0:pt} ${ln:pt}"
+        (( pt++ ))
+        return
+    fi
+
+    # do stuff here
     make run
-    echo -n "${PS1@P}" # print prompt again
+
+    # no need to re-print the prompt
 }
 
-complete -F cmd_complete -E
-
-bind "Space:complete"
+bind -x '" ":_hydra'
